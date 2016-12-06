b08e5a4e31ca2bfe42169d39ad9dbe5d91903d9e
dmitriy spivakov <dmitriy.spiv@gmail.com>
2016-12-06 16:29:42 +0200

    Add validations in db

--- db/migrate/20161206130507_create_dispatchers.rb
+++ db/migrate/20161206130507_create_dispatchers.rb
@@ -1,9 +1,9 @@
 class CreateDispatchers < ActiveRecord::Migration[5.0]
   def change
     create_table :dispatchers do |t|
-      t.string :email
-      t.string :password
-      t.string :name
+      t.string :email, :unique => true, :null => false
+      t.string :password, :null => false
+      t.string :name, :null => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20161206130507) do
   enable_extension "plpgsql"
 
   create_table "dispatchers", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.string   "name"
+    t.string   "email",      null: false
+    t.string   "password",   null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
