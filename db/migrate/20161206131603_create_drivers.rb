9379dc232278dd6cfbc5c95035ce31b2cf90d8cc
domenic99 <alexander95giz@gmail.com>
2016-12-06 16:19:29 +0200

    Chenged all values to not nullable. Added uniq to phone

--- db/migrate/20161206131603_create_drivers.rb
+++ db/migrate/20161206131603_create_drivers.rb
@@ -1,13 +1,15 @@
 class CreateDrivers < ActiveRecord::Migration[5.0]
   def change
     create_table :drivers do |t|
-      t.string :name
-      t.string :phone
-      t.string :pass
-      t.string :auto
-      t.string :status
+      t.string :name,   null: false
+      t.string :phone,  null: false
+      t.string :pass,   null: false
+      t.string :auto,   null: false
+      t.string :status, default: "not active"
 
       t.timestamps
     end
+
+    add_index :drivers, :phone, unique: true
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161206131603) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "email"
+    t.string   "phone",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["phone"], name: "index_clients_on_phone", unique: true, using: :btree
+  end
+
+  create_table "drivers", force: :cascade do |t|
+    t.string   "name",                              null: false
+    t.string   "phone",                             null: false
+    t.string   "pass",                              null: false
+    t.string   "auto",                              null: false
+    t.string   "status",     default: "not active"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.index ["phone"], name: "index_drivers_on_phone", unique: true, using: :btree
+  end
+
+end
