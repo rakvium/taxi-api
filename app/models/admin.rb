13d8ce2f22242e2a7ba8a321027ad3c342a8f84b
abapolov <abapolov.vlad@gmail.com>
2016-12-07 11:52:19 +0200

    Add index migration create admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- db/migrate/20161206125911_create_admins.rb
+++ db/migrate/20161206125911_create_admins.rb
@@ -2,11 +2,13 @@
 class CreateAdmins < ActiveRecord::Migration[5.0]
   def change
     create_table :admins do |t|
-      t.string :name
-      t.string :email, unique: true, null: false
+      t.string :name, null: false
+      t.string :email, null: false
       t.string :password, null: false
 
       t.timestamps
     end
+
+    add_index :admins, :email, unique: true
   end
 end
