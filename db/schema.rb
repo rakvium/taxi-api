d19fe7a2d568402b53589ab8643240201a0eb081
Vlad <abapolov.vlad@gmail.com>
2016-12-07 17:43:14 +0200

    add admin login with devise (#22)
    * add admin login with devise
    
    * remove long line and add exclusion to rubocop config file
    
    * exclude files into rubocop config
    
    * Write better comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207105344) do
+ActiveRecord::Schema.define(version: 20161207125550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "name",                               null: false
+    t.string   "email",                              null: false
+    t.string   "password",                           null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "email"
+    t.string   "phone",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["phone"], name: "index_clients_on_phone", unique: true, using: :btree
+  end
 
   create_table "dispatchers", force: :cascade do |t|
     t.string   "email",      null: false
@@ -23,21 +48,6 @@ ActiveRecord::Schema.define(version: 20161207105344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_dispatchers_on_email", unique: true, using: :btree
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "email",      null: false
-    t.string   "password",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
-
-  create_table "clients", force: :cascade do |t|
-    t.string   "email"
-    t.string   "phone",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["phone"], name: "index_clients_on_phone", unique: true, using: :btree
   end
 
   create_table "drivers", force: :cascade do |t|
