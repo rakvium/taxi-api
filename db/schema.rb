e8e8e6bd65d845113ecac858e247a5b266f43fa7
Vlad <abapolov.vlad@gmail.com>
2016-12-07 13:47:12 +0200

    Added admin model (#19)
    * Create model admin
    
    * Fixed problem with validation and spelling mistakes
    
    * we couldn`t have admin without name and email
    
    * Now couldn`t have any admin without email
    
    * Add index migration create admins
    
    * Add index email uniq

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206131603) do
+ActiveRecord::Schema.define(version: 20161207105344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.string   "password",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
 
   create_table "clients", force: :cascade do |t|
     t.string   "email"
