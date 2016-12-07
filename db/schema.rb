adede30d8c5e6e16499193690906333a08beebb8
abapolov <abapolov.vlad@gmail.com>
2016-12-07 13:02:12 +0200

    Add index email uniq

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206125911) do
+ActiveRecord::Schema.define(version: 20161207105344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.string   "email",      null: false
     t.string   "password",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
   end
 
 end
