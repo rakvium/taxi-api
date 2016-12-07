2666a797d95bbfb55e8dda4bb1c261baf861c66f
dmitriy spivakov <dmitriy.spiv@gmail.com>
2016-12-07 18:17:43 +0200

    fix conflict

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,7 @@
+# model for admins, this model can CRUD for all models in the system
 class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
