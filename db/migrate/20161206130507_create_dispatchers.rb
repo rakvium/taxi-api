182a6a0bd4b52ef368bc83e960c3d827e4a8d279
dmitriy spivakov <dmitriy.spiv@gmail.com>
2016-12-07 11:35:27 +0200

    Fix hash syntax in migration

--- db/migrate/20161206130507_create_dispatchers.rb
+++ db/migrate/20161206130507_create_dispatchers.rb
@@ -1,9 +1,9 @@
 class CreateDispatchers < ActiveRecord::Migration[5.0]
   def change
     create_table :dispatchers do |t|
-      t.string :email, :null => false
-      t.string :password, :null => false
-      t.string :name, :null => false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
