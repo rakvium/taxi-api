f0853fd0231d02c0b9f9b61f13978dfc145e1a9b
domenic99 <alexander95giz@gmail.com>
2016-12-08 13:24:39 +0200

    Deleted wrong line

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 
 ActiveRecord::Schema.define(version: 20161207125550) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
