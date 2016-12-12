3b7a29f87056937845092dffa498c8597da48b85
Victor Rak <rakvium@gmail.com>
2016-12-13 00:03:28 +0200

    RSpec: set colored output with documentation formatter

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Client, type: :model do
   context 'validation' do
-    it 'should be valid' do
+    it 'should be valid when created by the fabric' do
       expect(create(:client)).to be_valid
     end
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   context 'validation' do
-    it 'should be valid' do
+    it 'should be valid when created by the fabric' do
       expect(create(:order)).to be_valid
     end
   end
