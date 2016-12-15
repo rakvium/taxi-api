d86742afa6995213358758b1cd84ff6a6e3d6792
abapolov <abapolov.vlad@gmail.com>
2016-12-15 13:07:31 +0200

    successfull work drivers login and admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-
   devise_for :drivers # defaults: { format: :json } # undefined method `flash'
   post 'auth_driver' => 'authentication#authenticate_driver'
   post 'auth_admin' => 'admin_authentication#authenticate_admin'
   get 'home' => 'home#index'
+  get 'admin' => 'admin#index'
   resources :orders, only: [:index, :create]
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
