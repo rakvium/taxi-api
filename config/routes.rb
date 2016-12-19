e3daec2956a8bc8bde476c9f8ba942814990bd19
domenic99 <alexander95giz@gmail.com>
2016-12-19 19:30:02 +0200

    Added specs for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  #devise_for :dispatchers
-  #devise_for :drivers # defaults: { format: :json } # undefined method `flash'
+  # devise_for :dispatchers
+  # devise_for :drivers # defaults: { format: :json } # undefined method `flash'
   post 'auth_driver' => 'authentication#authenticate_driver'
   post 'auth_admin' => 'admin_authentication#authenticate_admin'
   get 'home' => 'home#index'
   get 'admin' => 'admin#index'
   resources :orders, only: [:index, :create, :update]
   resources :dispatchers, only: [:index, :create]
-  #devise_for :admins
+  # devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
