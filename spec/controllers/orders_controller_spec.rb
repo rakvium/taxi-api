b0d0f4766e041afb5e92c9c6a03a2ddbd3d8b42e
domenic99 <alexander95giz@gmail.com>
2016-12-20 11:55:49 +0200

    Feature/dispatcher login (#44)
    * Created new model for Dispatcher
    
    * some little changes
    
    * Added specs for orders
    
    * Added specs for dispatcher controller
    
    * Fixed rubocop offenses
    
    * Deleted commented code

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -9,11 +9,25 @@ RSpec.describe OrdersController, type: :controller do
   end
 
   context 'POST #create' do
-    it 'when all parameters is good' do
+    let(:order) { FactoryGirl.attributes_for(:order) }
+    let(:client) { FactoryGirl.attributes_for(:client) }
+
+    it 'when all parameters are good' do
       expect do
-        post :create, params: { order: FactoryGirl.attributes_for(:order),
-                                client: FactoryGirl.attributes_for(:client) }
+        post :create, params: { order: order, client: client }
       end.to change(Order, :count).by(1)
+    end
+
+    it 'when wrong order parameter' do
+      order[:from] = ''
+      post :create, params: { order: order, client: client }
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'when wrong client parameter' do
+      client[:phone] = ''
+      post :create, params: { order: order, client: client }
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
