bbff7f4b08c98a3579d58ce30094407d73fa1db2
Vlad <abapolov.vlad@gmail.com>
2016-12-21 14:35:32 +0200

    Admin can create users (#46)
    * Admin can create users
    
    * minimize routes and fix output errors
    
    * add rspec tests for create users
    
    * fix rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # devise_for :drivers # defaults: { format: :json } # undefined method `flash'
   post 'auth_driver' => 'authentication#authenticate_driver'
   post 'auth_admin' => 'admin_authentication#authenticate_admin'
+  post 'create_admin' => 'admin#create_admin'
+  post 'create_driver' => 'admin#create_driver'
+  post 'create_dispatcher' => 'admin#create_dispatcher'
   get 'home' => 'home#index'
   get 'admin' => 'admin#index'
   resources :orders, only: [:index, :create, :update]
--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :admin do
     name { Faker::Name.first_name }
     email { Faker::Internet.safe_email }
-    password { Faker::Internet.password(6, 12) }
+    encrypted_password { Faker::Internet.password(6, 12) }
   end
 end
