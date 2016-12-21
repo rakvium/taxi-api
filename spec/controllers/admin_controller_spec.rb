9cc81623b32510b1eb5b0951398b892a6a836deb
abapolov <abapolov.vlad@gmail.com>
2016-12-21 14:25:15 +0200

    fix rubocop

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -10,7 +10,11 @@ RSpec.describe AdminController, type: :controller do
       current_admin = Admin.create FactoryGirl.attributes_for(:admin)
       token = JsonWebToken.encode(user_id: current_admin.id, type: 'admin')
       request.headers['Authorization'] = token
-      post :create_admin, format: :json, params: { admin: { name: 'admin', email: 'admin@email.com', password: 'password' } }
+      post :create_admin, format: :json, params: { admin: {
+        name: 'admin',
+        email: 'admin@email.com',
+        password: 'password'
+      } }
       expect(JSON.parse(response.body)).to include_json(name: 'admin', email: 'admin@email.com')
     end
   end
@@ -26,21 +30,35 @@ RSpec.describe AdminController, type: :controller do
       current_admin = Admin.create FactoryGirl.attributes_for(:admin)
       token = JsonWebToken.encode(user_id: current_admin.id, type: 'admin')
       request.headers['Authorization'] = token
-      post :create_dispatcher, format: :json, params: { dispatcher: { name: 'dispatcher', email: 'dispatcher@email.com', password: 'password' } }
+      post :create_dispatcher, format: :json, params: { dispatcher: {
+        name: 'dispatcher',
+        email: 'dispatcher@email.com',
+        password: 'password'
+      } }
       expect(JSON.parse(response.body)).to include_json(name: 'dispatcher', email: 'dispatcher@email.com')
     end
   end
 
   context 'POST #create_driver' do
     it 'can`t create driver if not authenticated as an admin' do
-      post :create_driver, format: :json, params: { driver: { name: 'vasya', phone: '123456', password: '123123', auto: 'bmw' } }
+      post :create_driver, format: :json, params: { driver: {
+        name: 'vasya',
+        phone: '123456',
+        password: '123123',
+        auto: 'bmw'
+      } }
       expect(JSON.parse(response.body)).to include_json(errors: ['Not Authenticated'])
     end
     it 'can create driver' do
       current_admin = Admin.create FactoryGirl.attributes_for(:admin)
       token = JsonWebToken.encode(user_id: current_admin.id, type: 'admin')
       request.headers['Authorization'] = token
-      post :create_driver, format: :json, params: { driver: { name: 'vasya', phone: '123456', password: '123123', auto: 'bmw' } }
+      post :create_driver, format: :json, params: { driver: {
+        name: 'vasya',
+        phone: '123456',
+        password: '123123',
+        auto: 'bmw'
+      } }
       expect(JSON.parse(response.body)).to include_json(name: 'vasya', phone: '123456', auto: 'bmw')
     end
   end
