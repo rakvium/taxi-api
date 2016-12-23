c4d6259611a0000feaf9f7dff69cba015ebd470e
Vlad <abapolov.vlad@gmail.com>
2016-12-23 20:56:07 +0200

    add send mail for cancel and complete orders (#64)

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -3,6 +3,18 @@ class ClientMailer < ApplicationMailer
   default from: 'taxi@taxi.com'
 
   def welcome_email(order_id, email)
+    @order = Order.find(order_id)
+    @auto = Driver.find(Order.find(order_id).driver_id).auto
+    mail(to: email, subject: 'Taxi')
+  end
+
+  def cancel_email(order_id, email)
+    @order = Order.find(order_id)
+    mail(to: email, subject: 'Taxi')
+  end
+
+  def complete_email(order_id, email)
+    @order = Order.find(order_id)
     @auto = Driver.find(Order.find(order_id).driver_id).auto
     mail(to: email, subject: 'Taxi')
   end
