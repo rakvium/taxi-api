099933b21de0a318112c63c9c289e529d07d23be
abapolov <abapolov.vlad@gmail.com>
2016-12-23 18:45:55 +0200

    now can cancel order when driver dont apply it

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -9,7 +9,6 @@ class ClientMailer < ApplicationMailer
 
   def cancel_email(order_id, email)
     @order = Order.find(order_id)
-    @auto = Driver.find(Order.find(order_id).driver_id).auto
     mail(to: email, subject: 'Taxi')
   end
 end
