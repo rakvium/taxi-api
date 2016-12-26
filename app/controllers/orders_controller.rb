e6254448cc86a7f06bbac38db586f067ff3f8ed8
domenic99 <alexander95giz@gmail.com>
2016-12-26 11:18:35 +0200

    Changed driver_id to id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_request!, only: [:index, :show, :update, :cancel, :apply, :complete]
 
   def index
-    render json: { 'driver_id' => @current_user.id, 'orders' => @current_user.show_order_list }
+    render json: { 'id' => @current_user.id, 'orders' => @current_user.show_order_list }
   end
 
   # here i should create order from params
